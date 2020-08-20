c6037418185789667184da88b30a5e77fbac8432
shinshin86 <beagles1986@gmail.com>
2020-08-20 09:11:13 +0900

    Setup prettier

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 Easily configure multiple CORS.
 
-
-
 ## Install
 
 I have yet to publish to npm.
@@ -15,8 +13,6 @@ npm install shinshin86/ecmcors
 # or
 yarn add shinshin86/ecmcors
 ```
-
-
 
 ## Usage
 
@@ -32,8 +28,6 @@ CORS_ALLOW_LIST=http://localhost:3000
 
 If you do not set the environment variables, all origins are not allowed.
 
-
-
 ## Example(express)
 
 ```javascript
@@ -44,10 +38,9 @@ const app = express();
 
 // Only authorized origin will be allowed to pass.
 app.get('/', ecmcors, (req, res) => {
-  res.status(200)
+  res.status(200);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 ```
-
--- test/index.js
+++ test/index.js
@@ -1,144 +1,143 @@
-
 const EventEmitter = require('events');
 const { expect } = require('chai');
 
 describe('ecmcors', () => {
-    const errorMessage = "Not allowed by CORS";
+  const errorMessage = 'Not allowed by CORS';
 
-    const fakeRequest = (method, headers) => {
-        return new FakeRequest(method, headers);
-    }
+  const fakeRequest = (method, headers) => {
+    return new FakeRequest(method, headers);
+  };
 
-    const fakeResponse = () => {
-        return new FakeResponse();
-    }
+  const fakeResponse = () => {
+    return new FakeResponse();
+  };
 
-    describe('Not set CORS_ALLOW_LIST', () => {
-        beforeEach(() => {
-            process.env.CORS_ALLOW_LIST = ""      
-        });
-
-        it("Don't set the environment variables, all origins are not allowed.", done => {            
-            const ecmcors = require('../index');
-
-            const req = fakeRequest('GET');
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error.toString()).to.include(errorMessage)
-                done();
-            });
-        })
-    })
-
-    describe('Set CORS_ALLOW_LIST (http://example.com)', () => {
-        let ecmcors;
-        beforeEach(() => {
-            process.env.CORS_ALLOW_LIST = "http://example.com"
-
-            ecmcors = require('../index');
-        });
-
-        it("Only authorized origin will be allowed to pass. (http://example.com)", done => {
-            const req = fakeRequest('GET');
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error).to.be.undefined;
-                done();
-            });
-        })
-    
-        it("Only authorized origin will be allowed to pass. (http://example.net is not allowed)", done => {
-            const req = fakeRequest('GET', {
-                'origin': 'http://example.net'
-            });
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error.toString()).to.include(errorMessage)
-                done();
-            });
-        })
+  describe('Not set CORS_ALLOW_LIST', () => {
+    beforeEach(() => {
+      process.env.CORS_ALLOW_LIST = '';
     });
 
-    describe('Set multi origin CORS_ALLOW_LIST (http://example.com, http://example.net)', () => {
-        let ecmcors;
-        beforeEach(() => {
-            process.env.CORS_ALLOW_LIST = "http://example.com,http://example.net"
+    it("Don't set the environment variables, all origins are not allowed.", (done) => {
+      const ecmcors = require('../index');
 
-            ecmcors = require('../index');
-        });
+      const req = fakeRequest('GET');
+      const res = fakeResponse();
 
-        it("Only authorized origin will be allowed to pass. (http://example.com)", done => {
-            const req = fakeRequest('GET');
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error).to.be.undefined;
-                done();
-            });
-        })
-    
-        it("Only authorized origin will be allowed to pass. (http://example.net)", done => {
-            const req = fakeRequest('GET', {
-                'origin': 'http://example.net'
-            });
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error).to.be.undefined;
-                done();
-            });
-        })
-
-        it("Only authorized origin will be allowed to pass. (http://example.org is not allowed)", done => {
-            const req = fakeRequest('GET', {
-                'origin': 'http://example.org'
-            });
-            const res = fakeResponse();
-    
-            ecmcors(req, res, (error) => {
-                expect(error.toString()).to.include(errorMessage)
-                done();
-            });
-        })
+      ecmcors(req, res, (error) => {
+        expect(error.toString()).to.include(errorMessage);
+        done();
+      });
     });
-})
+  });
+
+  describe('Set CORS_ALLOW_LIST (http://example.com)', () => {
+    let ecmcors;
+    beforeEach(() => {
+      process.env.CORS_ALLOW_LIST = 'http://example.com';
+
+      ecmcors = require('../index');
+    });
+
+    it('Only authorized origin will be allowed to pass. (http://example.com)', (done) => {
+      const req = fakeRequest('GET');
+      const res = fakeResponse();
+
+      ecmcors(req, res, (error) => {
+        expect(error).to.be.undefined;
+        done();
+      });
+    });
+
+    it('Only authorized origin will be allowed to pass. (http://example.net is not allowed)', (done) => {
+      const req = fakeRequest('GET', {
+        origin: 'http://example.net',
+      });
+      const res = fakeResponse();
+
+      ecmcors(req, res, (error) => {
+        expect(error.toString()).to.include(errorMessage);
+        done();
+      });
+    });
+  });
+
+  describe('Set multi origin CORS_ALLOW_LIST (http://example.com, http://example.net)', () => {
+    let ecmcors;
+    beforeEach(() => {
+      process.env.CORS_ALLOW_LIST = 'http://example.com,http://example.net';
+
+      ecmcors = require('../index');
+    });
+
+    it('Only authorized origin will be allowed to pass. (http://example.com)', (done) => {
+      const req = fakeRequest('GET');
+      const res = fakeResponse();
+
+      ecmcors(req, res, (error) => {
+        expect(error).to.be.undefined;
+        done();
+      });
+    });
+
+    it('Only authorized origin will be allowed to pass. (http://example.net)', (done) => {
+      const req = fakeRequest('GET', {
+        origin: 'http://example.net',
+      });
+      const res = fakeResponse();
+
+      ecmcors(req, res, (error) => {
+        expect(error).to.be.undefined;
+        done();
+      });
+    });
+
+    it('Only authorized origin will be allowed to pass. (http://example.org is not allowed)', (done) => {
+      const req = fakeRequest('GET', {
+        origin: 'http://example.org',
+      });
+      const res = fakeResponse();
+
+      ecmcors(req, res, (error) => {
+        expect(error.toString()).to.include(errorMessage);
+        done();
+      });
+    });
+  });
+});
 
 class FakeRequest {
-    constructor(method, headers) {
-        this.headers = headers || {
-            'origin': 'http://example.com'
-        }
-    
-        this.method = method || 'GET'
-    }
+  constructor(method, headers) {
+    this.headers = headers || {
+      origin: 'http://example.com',
+    };
+
+    this.method = method || 'GET';
+  }
 }
 
 class FakeResponse extends EventEmitter {
-    constructor () {
-        super();
+  constructor() {
+    super();
 
-        this._headers = {}
-        this.statusCode = 200
-    }
+    this._headers = {};
+    this.statusCode = 200;
+  }
 
-    end() {
-        const response = this;
+  end() {
+    const response = this;
 
-        process.nextTick( () =>  {
-            response.emit('finish')
-          })
-    }
+    process.nextTick(() => {
+      response.emit('finish');
+    });
+  }
 
-    getHeader(name) {
-        const key = name.toLowerCase;
-        return this._headers[key];
-    }
+  getHeader(name) {
+    const key = name.toLowerCase;
+    return this._headers[key];
+  }
 
-    setHeader(name, value) {
-        const key = name.toLowerCase();
-        this._headers[key] = value;
-    }
+  setHeader(name, value) {
+    const key = name.toLowerCase();
+    this._headers[key] = value;
+  }
 }
